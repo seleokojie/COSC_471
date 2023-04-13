349785f49873df5cc5f518ae2c9d34d1298b27bc
Sele Okojie <seleokojie4@gmail.com>
2023-04-13 14:02:56 -0400

    Refactored Code

--- Assignment_3/code/rasterizer.cpp
+++ Assignment_3/code/rasterizer.cpp
@@ -10,51 +10,59 @@
 #include <math.h>
 
 
-rst::pos_buf_id rst::rasterizer::load_positions(const std::vector<Eigen::Vector3f> &positions)
-{
+rst::pos_buf_id rst::rasterizer::load_positions(const std::vector<Eigen::Vector3f> &positions){
     auto id = get_next_id();
     pos_buf.emplace(id, positions);
 
     return {id};
 }
 
-rst::ind_buf_id rst::rasterizer::load_indices(const std::vector<Eigen::Vector3i> &indices)
-{
+rst::ind_buf_id rst::rasterizer::load_indices(const std::vector<Eigen::Vector3i> &indices){
     auto id = get_next_id();
     ind_buf.emplace(id, indices);
 
     return {id};
 }
 
-rst::col_buf_id rst::rasterizer::load_colors(const std::vector<Eigen::Vector3f> &cols)
-{
+rst::col_buf_id rst::rasterizer::load_colors(const std::vector<Eigen::Vector3f> &cols){
     auto id = get_next_id();
     col_buf.emplace(id, cols);
 
     return {id};
 }
 
-auto to_vec4(const Eigen::Vector3f& v3, float w = 1.0f)
-{
+auto to_vec4(const Eigen::Vector3f& v3, float w = 1.0f){
     return Vector4f(v3.x(), v3.y(), v3.z(), w);
 }
 
 
-static bool insideTriangle(int x, int y, const Vector3f* _v)
-{   
-    // TODO : Implement this function to check if the point (x, y) is inside the triangle represented by _v[0], _v[1], _v[2]
+static bool insideTriangle(int x, int y, const Vector3f* _v){   
+    // Bounding box
+    float minX = std::min(_v[0].x(), std::min(_v[1].x(), _v[2].x()));
+    float maxX = std::max(_v[0].x(), std::max(_v[1].x(), _v[2].x()));
+    float minY = std::min(_v[0].y(), std::min(_v[1].y(), _v[2].y()));
+    float maxY = std::max(_v[0].y(), std::max(_v[1].y(), _v[2].y()));
+    if (x < minX || x > maxX || y < minY || y > maxY) {
+        return false;
+    }
+
+    // Barycentric coordinates
+    auto [c1, c2, c3] = computeBarycentric2D(x, y, _v);
+    if (c1 < 0 || c2 < 0 || c3 < 0) {
+        return false;
+    }
+
+    return true;
 }
 
-static std::tuple<float, float, float> computeBarycentric2D(float x, float y, const Vector3f* v)
-{
+static std::tuple<float, float, float> computeBarycentric2D(float x, float y, const Vector3f* v){
     float c1 = (x*(v[1].y() - v[2].y()) + (v[2].x() - v[1].x())*y + v[1].x()*v[2].y() - v[2].x()*v[1].y()) / (v[0].x()*(v[1].y() - v[2].y()) + (v[2].x() - v[1].x())*v[0].y() + v[1].x()*v[2].y() - v[2].x()*v[1].y());
     float c2 = (x*(v[2].y() - v[0].y()) + (v[0].x() - v[2].x())*y + v[2].x()*v[0].y() - v[0].x()*v[2].y()) / (v[1].x()*(v[2].y() - v[0].y()) + (v[0].x() - v[2].x())*v[1].y() + v[2].x()*v[0].y() - v[0].x()*v[2].y());
     float c3 = (x*(v[0].y() - v[1].y()) + (v[1].x() - v[0].x())*y + v[0].x()*v[1].y() - v[1].x()*v[0].y()) / (v[2].x()*(v[0].y() - v[1].y()) + (v[1].x() - v[0].x())*v[2].y() + v[0].x()*v[1].y() - v[1].x()*v[0].y());
     return {c1,c2,c3};
 }
 
-void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type)
-{
+void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type){
     auto& buf = pos_buf[pos_buffer.pos_id];
     auto& ind = ind_buf[ind_buffer.ind_id];
     auto& col = col_buf[col_buffer.col_id];
@@ -63,8 +71,7 @@ void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf
     float f2 = (50 + 0.1) / 2.0;
 
     Eigen::Matrix4f mvp = projection * view * model;
-    for (auto& i : ind)
-    {
+    for (auto& i : ind){
         Triangle t;
         Eigen::Vector4f v[] = {
                 mvp * to_vec4(buf[i[0]], 1.0f),
@@ -76,15 +83,13 @@ void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf
             vec /= vec.w();
         }
         //Viewport transformation
-        for (auto & vert : v)
-        {
+        for (auto & vert : v){
             vert.x() = 0.5*width*(vert.x()+1.0);
             vert.y() = 0.5*height*(vert.y()+1.0);
             vert.z() = vert.z() * f1 + f2;
         }
 
-        for (int i = 0; i < 3; ++i)
-        {
+        for (int i = 0; i < 3; ++i){
             t.setVertex(i, v[i].head<3>());
             t.setVertex(i, v[i].head<3>());
             t.setVertex(i, v[i].head<3>());
@@ -118,23 +123,19 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
     // TODO : set the current pixel (use the set_pixel function) to the color of the triangle (use getColor function) if it should be painted.
 }
 
-void rst::rasterizer::set_model(const Eigen::Matrix4f& m)
-{
+void rst::rasterizer::set_model(const Eigen::Matrix4f& m){
     model = m;
 }
 
-void rst::rasterizer::set_view(const Eigen::Matrix4f& v)
-{
+void rst::rasterizer::set_view(const Eigen::Matrix4f& v){
     view = v;
 }
 
-void rst::rasterizer::set_projection(const Eigen::Matrix4f& p)
-{
+void rst::rasterizer::set_projection(const Eigen::Matrix4f& p){
     projection = p;
 }
 
-void rst::rasterizer::clear(rst::Buffers buff)
-{
+void rst::rasterizer::clear(rst::Buffers buff){
     if ((buff & rst::Buffers::Color) == rst::Buffers::Color)
     {
         std::fill(frame_buf.begin(), frame_buf.end(), Eigen::Vector3f{0, 0, 0});
@@ -145,19 +146,16 @@ void rst::rasterizer::clear(rst::Buffers buff)
     }
 }
 
-rst::rasterizer::rasterizer(int w, int h) : width(w), height(h)
-{
+rst::rasterizer::rasterizer(int w, int h) : width(w), height(h){
     frame_buf.resize(w * h);
     depth_buf.resize(w * h);
 }
 
-int rst::rasterizer::get_index(int x, int y)
-{
+int rst::rasterizer::get_index(int x, int y){
     return (height-1-y)*width + x;
 }
 
-void rst::rasterizer::set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color)
-{
+void rst::rasterizer::set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color){
     //old index: auto ind = point.y() + point.x() * width;
     auto ind = (height-1-point.y())*width + point.x();
     frame_buf[ind] = color;
--- Assignment_3/code/rasterizer.hpp
+++ Assignment_3/code/rasterizer.hpp
@@ -10,26 +10,21 @@
 #include "Triangle.hpp"
 using namespace Eigen;
 
-namespace rst
-{
-    enum class Buffers
-    {
+namespace rst{
+    enum class Buffers{
         Color = 1,
         Depth = 2
     };
 
-    inline Buffers operator|(Buffers a, Buffers b)
-    {
+    inline Buffers operator|(Buffers a, Buffers b){
         return Buffers((int)a | (int)b);
     }
 
-    inline Buffers operator&(Buffers a, Buffers b)
-    {
+    inline Buffers operator&(Buffers a, Buffers b){
         return Buffers((int)a & (int)b);
     }
 
-    enum class Primitive
-    {
+    enum class Primitive{
         Line,
         Triangle
     };
@@ -39,23 +34,19 @@ namespace rst
      * make sure that if you mix up with their orders, the compiler won't compile it.
      * Aka : Type safety
      * */
-    struct pos_buf_id
-    {
+    struct pos_buf_id{
         int pos_id = 0;
     };
 
-    struct ind_buf_id
-    {
+    struct ind_buf_id{
         int ind_id = 0;
     };
 
-    struct col_buf_id
-    {
+    struct col_buf_id{
         int col_id = 0;
     };
 
-    class rasterizer
-    {
+    class rasterizer{
     public:
         rasterizer(int w, int h);
         pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
